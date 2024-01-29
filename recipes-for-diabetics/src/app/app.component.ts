4be8a03b238873553a7a1a64099af083fff9f69b
Vinay Avasthi <vinay@avasthi.com>
2024-01-29 23:07:03 +0530

    Adding themes.

--- recipes-for-diabetics/src/app/app.component.ts
+++ recipes-for-diabetics/src/app/app.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RecipeComponent } from './recipe/recipe.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RecipeComponent],
+  imports: [CommonModule,
+    RouterOutlet,
+    RecipeComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
