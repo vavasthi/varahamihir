3e345f4b38e932c8cdbaaf6ffdef13bed1fff0a5
Vinay Avasthi <vinay@avasthi.com>
2024-07-20 17:33:56 +0530

    Removed excessive log statements.

--- our-life/src/app/interceptors/authentication.interceptor.ts
+++ our-life/src/app/interceptors/authentication.interceptor.ts
@@ -8,11 +8,9 @@ import { RequestAuthOption } from '../pojo/request-auth-option';
 export class AuthenticationInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>,
     handler: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Request URL: ' + req.url);
     var authService = inject(AuthService)
     if (req.context.get(RequestAuthOption.NONE)) {
 
-      console.log("A NONE request received..");
       return handler.handle(req).pipe(
         catchError((error: HttpErrorResponse) => {
 
@@ -24,25 +22,21 @@ export class AuthenticationInterceptor implements HttpInterceptor {
 
       var authToken = 'Bearer ' + authService.getCurrentUser()()?.refreshToken
       var newHeaders = req.headers.append('Authorization', authToken).append("Content-Type", "application/json");
-      console.log("A refresh token request received.. with token " + authToken);
       req.context.set(RequestAuthOption.REFRESH_TOKEN, false);
       return handler.handle(req.clone({headers: newHeaders, context:req.context}));
     }
     else if (req.context.get(RequestAuthOption.AUTH_TOKEN)) {
       var authToken = 'Bearer ' + authService.getCurrentUser()()?.authToken
-      var newHeaders = req.headers.append('Authorization', 'Bearer ' + authToken)
-      console.log("A regular auth token request received.. with auth token " + authToken);
+      var newHeaders = req.headers.append('Authorization', authToken)
       var originalRequest = req.clone();
       return handler.handle(req.clone({ headers: newHeaders })).pipe(
         catchError((error: HttpErrorResponse) => {
 
           if (error.status == 403) {
-            console.log("Looks like auth token expired.. refreshing token..");
             authService.refresh();
             originalRequest.context.set(RequestAuthOption.AUTH_TOKEN, true);
             var authToken = 'Bearer ' + authService.getCurrentUser()()?.authToken
             var newHeaders = originalRequest.headers.append('Authorization', authToken)
-            console.log("Retrying original regular auth token request with refreshed authtoken.. with auth token " + authToken + " " + originalRequest.url);
             return handler.handle(originalRequest.clone({ headers: newHeaders, context:originalRequest.context }));
           }
           return throwError(() => error)
@@ -50,7 +44,6 @@ export class AuthenticationInterceptor implements HttpInterceptor {
       );
     }
     else {
-      console.log("A default request received..");
       return handler.handle(req);
     }
   }
